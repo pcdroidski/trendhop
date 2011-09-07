2ef95f600b705bbb03429ccf2f0fe0077025e61c
Patrick <pcedrowski@gmail.com>
2011-09-06 21:22:55 -0400

    more work on searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,29 @@ class SearchController < ApplicationController
     @trends = Trend.search(@search)
     @posts = Post.search(@search)
     @users = User.search(@search)
+    
+    model_array = ["@trends", "@posts", "@users"]
+    select_array = [@trends.count, @posts.count, @users.count]
+    max = nil
+    model_array.each do |set|
+      if eval(set.count) >= select_array.max
+        max = set
+      end
+    end
+    raise max.inspect
     @search_view = params[:search_view].blank? ? "trends" : params[:search_view].to_s
   end
-
+  
+  def match_and_count(view)
+    case view
+    when "posts"
+      return @posts.count
+    when "users"
+      return @users.count
+    when "trends"
+      return @trends.count
+    end
+  end
+  helper_method :match_and_count
+  
 end
